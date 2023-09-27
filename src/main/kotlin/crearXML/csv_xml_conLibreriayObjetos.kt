292545763ed970c2622e3c4e0fd3c6b37f7c52be
LuciaES24 <lespsan543@g.educaand.es>
2023-09-27 13:06:53 +0200

    crear xml con objetos y libreria con excepciones

--- src/main/kotlin/crearXML/csv_xml_conLibreriayObjetos.kt
+++ src/main/kotlin/crearXML/csv_xml_conLibreriayObjetos.kt
@@ -2,37 +2,45 @@ package crearXML
 
 import com.github.doyaaaaaken.kotlincsv.client.CsvReader
 import java.io.File
+import java.io.FileNotFoundException
 
 fun main(){
-    val tiempoInicio = System.currentTimeMillis()
+    try {
+        val csv = File("itinerarios.csv")
+        val csvReader = CsvReader()
+        val contenido = csvReader.readAllWithHeader(csv)
+        if (contenido.isEmpty()){
+            throw FileNotFoundException()
+        }else{
+            val tiempoInicio = System.currentTimeMillis()
 
-    val csv = File("itinerarios.csv")
-    val csvReader = CsvReader()
-    val contenido = csvReader.readAllWithHeader(csv)
+            //Creamos el archivo xml
+            val xml_csv = File("xml_csv.xml")
 
-    //Creamos el archivo xml
-    val xml_csv = File("xml_csv.xml")
+            //Creamos un objeto XML
+            val xml = XML()
+            xml.anadirCabecera()
+            xml.anadirPadre("Archivo")
 
-    //Creamos un objeto XML
-    val xml = XML()
-    xml.anadirCabecera()
-    xml.anadirPadre("Archivo")
+            //Recorremos la lista que hemos extraido del csv
+            for (linea in contenido){
+                xml.anadirElemento("Registro")
+                //Recorremos el diccionario de cada línea y lo añadimos como elemento hijo
+                for ((key, value) in linea){
+                    xml.anadirElementoHijoInformacion(key,value)
+                }
+                xml.cierreElemento("Registro")
+            }
+            xml.cierrePadre("Archivo")
 
-    //Recorremos la lista que hemos extraido del csv
-    for (linea in contenido){
-        xml.anadirElemento("Registro")
-        //Recorremos el diccionario de cada línea y lo añadimos como elemento hijo
-        for ((key, value) in linea){
-            xml.anadirElementoHijoInformacion(key,value)
+            //Escribimos en el archivo xml que creamos previamente
+            xml_csv.writeText(xml.lineas_xml)
+
+            val tiempoFin = System.currentTimeMillis()
+            val tiempoTotal = tiempoFin-tiempoInicio
+            println("El tiempo es de "+ tiempoTotal + " milisegundos")
         }
-        xml.cierreElemento("Registro")
+    }catch (e:FileNotFoundException){
+        println("El archivo no se ha encontrado o está vacío")
     }
-    xml.cierrePadre("Archivo")
-
-    //Escribimos en el archivo xml que creamos previamente
-    xml_csv.writeText(xml.lineas_xml)
-
-    val tiempoFin = System.currentTimeMillis()
-    val tiempoTotal = tiempoFin-tiempoInicio
-    println("El tiempo es de "+ tiempoTotal + " milisegundos")
 }
--- src/main/kotlin/crearXML/csv_xml_conObjetos.kt
+++ src/main/kotlin/crearXML/csv_xml_conObjetos.kt
@@ -1,44 +1,52 @@
 package crearXML
 
 import java.io.File
+import java.io.FileNotFoundException
 
 fun main() {
-    val tiempoInicio = System.currentTimeMillis()
+    try {
+        //Creamos el csv y estraemos su información
+        val csv = File("itinerarios.csv").inputStream()
+        val bufferedReader = csv.bufferedReader()
+        val lines = bufferedReader.readLines()
+        if (lines.isEmpty()){
+            throw FileNotFoundException()
+        }else{
+            val tiempoInicio = System.currentTimeMillis()
 
-    //Creamos el csv y estraemos su información
-    val csv = File("itinerarios.csv").inputStream()
-    val bufferedReader = csv.bufferedReader()
-    val lines = bufferedReader.readLines()
+            //Guardamos la primera línea en la que estarán los nombres de los elementos
+            val entities = lines[0].split(",")
 
-    //Guardamos la primera línea en la que estarán los nombres de los elementos
-    val entities = lines[0].split(",")
+            //Creamos el archivo xml
+            val xml_csv = File("xml_csv.xml")
 
-    //Creamos el archivo xml
-    val xml_csv = File("xml_csv.xml")
+            //Creamos un objeto XML
+            val xml = XML()
+            xml.anadirCabecera()
+            xml.anadirPadre("Archivo")
 
-    //Creamos un objeto XML
-    val xml = XML()
-    xml.anadirCabecera()
-    xml.anadirPadre("Archivo")
+            //Vamos recorriendo las líneas del archivo csv que hemos leido
+            for (i in 1..lines.size-1){
+                xml.anadirElemento("Registro")
+                //Hacemos un split para separar por campos de informacion
+                var actualLine = lines[i].split(",")
+                //Recorremos los campos y añadimos el elemento hijo junto a la información que le corresponde
+                for (x in 0..actualLine.size-1){
+                    xml.anadirElementoHijoInformacion("${entities[x]}","${actualLine[x]}")
+                }
+                xml.cierreElemento("Registro")
+            }
 
-    //Vamos recorriendo las líneas del archivo csv que hemos leido
-    for (i in 1..lines.size-1){
-        xml.anadirElemento("Registro")
-        //Hacemos un split para separar por campos de informacion
-        var actualLine = lines[i].split(",")
-        //Recorremos los campos y añadimos el elemento hijo junto a la información que le corresponde
-        for (x in 0..actualLine.size-1){
-            xml.anadirElementoHijoInformacion("${entities[x]}","${actualLine[x]}")
+            xml.cierrePadre("Archivo")
+
+            //Escribimos en el archivo xml que creamos previamente
+            xml_csv.writeText(xml.lineas_xml)
+
+            val tiempoFin = System.currentTimeMillis()
+            val tiempoTotal = tiempoFin-tiempoInicio
+            println("El tiempo es de "+ tiempoTotal + " milisegundos")
         }
-        xml.cierreElemento("Registro")
+    }catch (e:FileNotFoundException){
+        println("El archivo no se ha encontrado o está vacío")
     }
-
-    xml.cierrePadre("Archivo")
-
-    //Escribimos en el archivo xml que creamos previamente
-    xml_csv.writeText(xml.lineas_xml)
-
-    val tiempoFin = System.currentTimeMillis()
-    val tiempoTotal = tiempoFin-tiempoInicio
-    println("El tiempo es de "+ tiempoTotal + " milisegundos")
 }
