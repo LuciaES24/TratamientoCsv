94eaad1ff1105e1bc5f7cd5963197c6cc6778e6e
LuciaES24 <lespsan543@g.educaand.es>
2023-10-05 12:24:59 +0200

    pasando xml con atributos a objetos

--- src/main/kotlin/crearObjetosdeXML/Elemento.kt
+++ src/main/kotlin/crearObjetosdeXML/Elemento.kt
@@ -10,4 +10,14 @@ class Elemento(nombre:String, info:String) {
         nombreAtributo = nombreAtt
         infoAtributo = infoAtt
     }
+
+    fun imprimirElemento():String{
+        var imprimir = ""
+        if (nombreAtributo == "" && infoAtributo == ""){
+            imprimir += "$nombreElemento: $infoElemento"
+        }else{
+            imprimir += "$nombreElemento: $infoElemento | $nombreAtributo: $infoAtributo"
+        }
+        return imprimir
+    }
 }
--- src/main/kotlin/crearObjetosdeXML/MainObjetosdeXML.kt
+++ src/main/kotlin/crearObjetosdeXML/MainObjetosdeXML.kt
@@ -5,7 +5,6 @@ import java.io.File
 import javax.lang.model.element.Element
 import javax.xml.parsers.DocumentBuilderFactory
 
-
 fun main(){
     val xml = File("itinerariosAtributo.xml")
     val dbFactory = DocumentBuilderFactory.newInstance()
@@ -16,11 +15,24 @@ fun main(){
     var listaElementos = mutableListOf<Elemento>()
 
     for (i in 0..nodeList.length-1){
-        var listaHijos = mutableListOf<Elemento>()
-        var nodo = nodeList.item(i)
-        var elemento = nodo.childNodes
-
+        var nodo = nodeList.item(i).childNodes
+        for (x in 0..nodo.length-1){
+            var nombreElemento = nodo.item(x).nodeName
+            var infoElemento = nodo.item(x).textContent
+            if (nodeList.item(x).hasAttributes()){
+                var atributo = nodeList.item(x).attributes.item(0).toString().split("=")
+                var nombreAtributo = atributo[0]
+                var infoAtributo = atributo[1]
+                var elemento = Elemento(nombreElemento,infoElemento,nombreAtributo,infoAtributo)
+                listaElementos.add(elemento)
+            }else{
+                var elemento = Elemento(nombreElemento,infoElemento)
+                listaElementos.add(elemento)
+            }
+        }
     }
 
-
+    for (elemento in listaElementos){
+        println(elemento.imprimirElemento())
+    }
 }
